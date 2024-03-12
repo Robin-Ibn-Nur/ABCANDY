8122f512ad5ad3f9cbd89b874848db576ad29af4
saifsammy <saifsammy653@gmail.com>
2024-03-12 17:56:11 +0600

    mobile and ai png setup successfully

--- app/Development/Ai.jsx
+++ app/Development/Ai.jsx
@@ -1,10 +1,12 @@
-import Image from 'next/image';
 import React from 'react';
 
 const Ai = () => {
     return (
-        <div className='w-[30%]'>
-            <Image className='h-auto w-full' src="/ai.png" sizes='100vw' width={500} height={500} />
+        <div className='p-5 lg:p-0 w-[100%] lg:w-[30%] bg-cover bg-center h-[650px] grid place-items-end ' style={{ backgroundImage: 'url(/ai.png)', padding: "10px", minHeight: "100%" }}>
+            <div className='flex flex-col bg-white mb-5 mx-5 p-10'>
+                <h1 className='text-[#2C2347] text-xl font-bold'>Do you need any creative service for your business?</h1>
+                <button className="text-xl w-32 rounded h-14 bg-[#5742AC] text-white relative overflow-hidden group z-10 hover:text-white duration-1000"><span className="absolute bg-[#5742AC] size-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span><span className="absolute bg-[#5742AC] size-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>Let's Talk</button>
+            </div>
         </div>
     );
 };
--- app/Development/Mobile.jsx
+++ app/Development/Mobile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Mobile = () => {
     return (
-        <div className="space-y-4 w-[70%]">
+        <div className="space-y-4 p-5 lg:p-0 w-[100%] lg:w-[70%]">
             <div className="space-y-2 ">
                 <Image className='h-auto w-full' src="/mobile.png" sizes='100vw' width={500} height={500} />
 
@@ -17,9 +17,7 @@ const Mobile = () => {
                     <h3 className="text-xl font-semibold text-[#2C2347]">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
                 </a>
                 <p className="dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
-                <div className="flex items-center gap-2">
-                    <span className='text-xs'>by</span><span>Jon Doe</span>
-                </div>
+                <button type="button" className="relative inline-block text-[#2C2347] font-bold h-14 w-40 overflow-hidden border-[#2C2347] px-5 py-2 shadow-lg before:absolute before:inset-0 before:-z-10 before:block before:translate-x-[90%] before:rounded-s-full before:bg-[#2C2347] before:duration-200 after:absolute after:inset-0 after:-z-10 after:block after:-translate-x-[90%] after:rounded-e-full after:bg-[#2C2347] after:duration-500 hover:text-white before:hover:translate-x-0 after:hover:translate-x-0">Read More</button>
             </div>
         </div>
     );
