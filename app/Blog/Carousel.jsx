48dbe20bdc1e64fbe020d02bc7b5bb6b4de6d364
saifsammy <saifsammy653@gmail.com>
2024-03-23 21:30:20 +0600

    slider images change

--- app/Blog/Carousel.jsx
+++ app/Blog/Carousel.jsx
@@ -5,11 +5,9 @@ const Carousel = () => {
     const { currentSlider, setCurrentSlider, sliderImages } = useCarousel();
     return (
         <div className="h-72 w-full rounded-md relative overflow-hidden">
-            {/* dots */}
             <Dots sliderImages={sliderImages} currentSlider={currentSlider} setCurrentSlider={setCurrentSlider} />
-            {/* Carousel container */}
             <div className="ease-linear duration-300 flex transform-gpu" style={{ transform: `translateX(-${currentSlider * 100}%)` }}>
-                {/* sliders */}
+
                 {sliderImages.map((slide, index) => (
                     <img key={index} src={slide} alt={`Slider - ${index + 1}`} className="min-w-full h-72 bg-black/20 sm:h-96 md:h-[670px] object-cover" />))}
             </div>
