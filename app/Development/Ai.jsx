187ee0b2bdde2ec35271ff7eb2fd1582db2e9069
Md. Robin <saifsammy653@gmail.com>
2024-03-24 13:07:11 +0000

    responsive

--- app/Development/Ai.jsx
+++ app/Development/Ai.jsx
@@ -4,7 +4,7 @@ import { Modal } from '../component/Modal';
 const Ai = () => {
     return (
         <div className='p-5 lg:p-0 w-[100%] lg:w-[30%] bg-cover bg-center h-[650px] relative ' style={{ backgroundImage: 'url(/ai.png)', padding: "10px", minHeight: "100%" }}>
-            <div className='absolute bottom-5  flex flex-col bg-white lg:ml-4 lg:mr-4 sm:p-[2%] lg:p-[5%]'>
+            <div className='absolute bottom-5 flex flex-col bg-white mx-auto lg:ml-4 lg:mr-4  sm:p-[2%] lg:p-[5%]'>
                 <h1 className='text-[#2C2347] text-xl font-bold mb-2'>Do you need any creative service for your business?</h1>
                 <Modal children={"Let's Talk"} />
             </div>
